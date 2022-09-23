b8c75b1eb53ab65e40ade9ddc1ba190e60570347
João Pedro Santiago <joaopedrosantiago1103@gmail.com>
2022-09-23 16:01:53 -0300

    new repository

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 800
+}).reveal(`#home,
+  #home img,
+  #home .description,
+  #about,
+  #about .content,
+  #about img,
+  #categories,
+  #categories .pots`)
